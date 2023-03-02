0718c30659f745f8d6e065c459f8c3f4f063dd94
cupoooo <admirsmajlovic703@gmail.com>
2023-03-02 15:04:46 +0100

    osnove js

---
+++ practice.js
@@ -0,0 +1,65 @@
+
+/* #1
+let myVar = 1;
+myVar +=5;
+
+console.log(myVar);
+*/
+
+/* Find the length of a string
+let LastNameLength = 0;
+const LastName = "Admir";
+
+console.log(LastNameLength = LastName.length);
+
+*/
+
+/* Find the character in a string
+const firstName = "Admir";
+const firstLetter = firstName[0];
+
+console.log(firstLetter);
+
+*/
+
+/* Finding the last character in a string
+const firstName = "Admir";
+lastCharacter = firstName [firstName.length - 1];
+
+console.log(lastCharacter);
+
+*/
+
+/* Findig index of array
+const myArray = [30,40,50];
+const myData = myArray[1];
+console.log(myData);
+*/
+
+/* Dodavanje u array
+const arr = [1,2,3];
+arr.push(4);
+
+console.log(arr);
+*/
+
+/* Pop oduzima zadnju vrijednost i arraya
+const arr = [10,20,30,40];
+arr.pop();
+console.log(arr);
+*/
+
+/* Shift oduzima prvu vijednost 
+const arr = [10,20,30,40];
+arr.shift();
+console.log(arr);
+*/
+
+/* Unshift dodaje vrijednos na pocetak niza
+const arr = [10,20,30,40];
+arr.unshift(5);
+console.log(arr);
+*/
+
+
+
